7f14a5af33cb51a174126255c136c8277a2ba4ea
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-17 00:20:40 +0200

    add unit test fix linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,6 @@ class Post < ApplicationRecord
   end
 
   validates :title, presence: true, allow_blank: false, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
