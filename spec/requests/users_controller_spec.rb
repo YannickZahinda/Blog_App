f438fe1f4f4395d7c0d3f9d507121f56ffa2e9a0
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-22 10:49:37 +0200

    fix rubocop offenses

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,37 +1,36 @@
 RSpec.describe 'Users', type: :request do
-    describe 'GET#index' do
-      it 'should have a status of 200' do
-        get users_path
+  describe 'GET#index' do
+    it 'should have a status of 200' do
+      get users_path
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should show template index' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+
+    it 'should have content on page that says -show for users' do
+      get users_path
+      expect(response.body).to include('Here is the list of users')
+    end
+  end
+
+  describe 'GET /show' do
+    context 'when the page is loaded' do
+      before do
+        get '/users/1'
+      end
+      it 'returns a 200 status code' do
         expect(response).to have_http_status(200)
       end
-  
-      it 'should show template index' do
-        get users_path
-        expect(response).to render_template(:index)
+      it 'should render correct template' do
+        expect(response).to render_template(:show)
       end
-  
-      it 'should have content on page that says -show for users' do
-        get users_path
-        expect(response.body).to include('Here is the list of users')
-      end
-    end
-  
-    describe 'GET /show' do
-      context 'when the page is loaded' do
-        before do
-          get '/users/1'
-        end
-        it 'returns a 200 status code' do
-          expect(response).to have_http_status(200)
-        end
-        it 'should render correct template' do
-          expect(response).to render_template(:show)
-        end
-  
-        it 'should include correct placeholder text' do
-          expect(response.body).to include('Here is a given specific user')
-        end
+
+      it 'should include correct placeholder text' do
+        expect(response.body).to include('Here is a given specific user')
       end
     end
   end
-  
+end
