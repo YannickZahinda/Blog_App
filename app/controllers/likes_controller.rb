70e058e3481b1c1d4eda9d7bcef022d2a5c26d10
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-23 19:06:34 +0200

    fix rubocop offenses

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
-    user = current_user
-    liked = Like.where(user:, post:)
+    user = current_user # rubocop:todo Lint/UselessAssignment
+    liked = Like.where(user: post_id, post: post_id)
 
     if liked.present?
       flash[:notice] = 'Post already liked'
     else
-      like = Like.create(user:, post:)
+      like = Like.create(user: post_id, post: post_id)
       if like.save
         redirect_to user_post_path(post.user, post), notice: 'Post successfully liked'
       else
