5fc3df13c913f3864df9eeca89910559649b97f5
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-23 19:01:35 +0200

    git add form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+  after_save :update_posts_counter
 
-  def self.update_counter(post_id)
-    count_comments = Comment.where(post_id: post_id).count
-    Post.find_by(id: post_id).update(commentsCounter: count_comments)
+  def update_posts_counter
+    user.increment!(:postsCounter)
   end
 end
