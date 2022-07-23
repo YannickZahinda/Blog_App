1c04b49b391cd00211b201c0a84b91cfcd5043d1
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-07-23 16:49:31 +0200

    fix rubocop offenses

--- db/migrate/20220608081530_change_column_name.rb
+++ db/migrate/20220608081530_change_column_name.rb
@@ -1,6 +1,6 @@
 class ChangeColumnName < ActiveRecord::Migration[7.0]
   def change
-# remove foreign key
+    # remove foreign key
     remove_foreign_key :posts, :users
     remove_foreign_key :comments, :users
     remove_foreign_key :comments, :posts
--- db/migrate/20220608104659_rename_column_name.rb
+++ db/migrate/20220608104659_rename_column_name.rb
@@ -6,13 +6,13 @@ class RenameColumnName < ActiveRecord::Migration[7.0]
     rename_column :likes, :authorId, :user_id
     rename_column :likes, :postId, :post_id
 
-# add back foreign keys
+    # add back foreign keys
     add_foreign_key :comments, :users, column: :user_id
     add_foreign_key :comments, :posts, column: :post_id
-   
+
     add_foreign_key :likes, :users, column: :user_id
     add_foreign_key :likes, :posts, column: :post_id
-   
+
     add_foreign_key :posts, :users, column: :user_id
   end
 end
